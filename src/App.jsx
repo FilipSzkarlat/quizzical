adc233c2380ef8f4a6dba8f75dcf64b591d8a491
Filip Szkarlat Szczepaniak <filip.szkarlat@gmail.com>
2025-07-07 10:37:03 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Quizzical</h1>
+      <p>Welcome to the Quizzical app!</p>
+      <button>Start Quiz</button>
+    </div>
+  );
+}
---
+++ src/StartingPage.jsx
@@ -0,0 +1,10 @@
+export default function StartingPage() {
+    return (
+        <div className="starting-page">
+            <h1>Quizzical</h1>
+            <p>Welcome to the Quizzical app!</p>
+            <button>Start quiz</button>
+        </div>
+    )
+}
+
